2bb4fc4332af11ee610de13ed484ee82bb67fc58
johnlim2025 <91999200+johnlim2025@users.noreply.github.com>
2022-12-08 18:03:23 -0600

    Add files via upload

--- comment.js
+++ comment.js
@@ -1,6 +1,7 @@
 function setObject(key, value) {
     window.localStorage.setItem(key, JSON.stringify(value));
   }
+  
   function getObject(key) {
     var storage = window.localStorage,
         value = storage.getItem(key);
